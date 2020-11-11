4348f4f3fb98992cf651b2befe96a874ba1eecd3
yunlong.wang <yunlong.wang@thoughtworks.com>
2020-11-11 16:29:35 +0800

    [yunlong] feat:finished all practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
-export { parseData };
+const parseData = (input) => {
+    const {data, column} = input;
+    return data.map(value => {
+        const Info = {};
+        column.map(value => value.name)
+            .forEach((prop, index) => {
+                Info[prop] = value[index];
+            });
+        return Info;
+    });
+}
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+    const revSections = sections.reverse();
+    console.log(revSections);
+    revSections.forEach(
+        section => {
+            items.splice(section.index, 0, section.content);
+        }
+    )
+    return items;
+}
+export {inject};
